f24158ff82308a2f26f1699f6b09ba9a20a74cd1
johnatans2 <johnatan.henrique14@hotmail.com>
2024-03-26 20:47:03 -0300

    Antigo exercício de Python

---
+++ teste.py
@@ -0,0 +1,15 @@
+#Apresentação
+print("Cálculo das Operações Fundamentais")
+#Inputs
+num1=float(input("Qual é o primeiro número? "))
+num2=float(input("Qual é o segundo número? "))
+#Processamento de Dados
+soma=num1+num2
+subtracao=num1-num2
+multiplicacao=num1*num2
+divisao=num1/num2
+#Outputs
+print(f"O total é {soma:.2f}")
+print(f"O resto é {subtracao:.2f}")
+print(f"O produto é {multiplicacao:.2f}")
+print(f"O quociente é {divisao:.2f}")
